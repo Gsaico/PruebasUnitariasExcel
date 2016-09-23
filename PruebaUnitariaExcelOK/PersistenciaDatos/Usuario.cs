34a66d0ad0d29b2a26cb502f826564a6c3f8258f
Percy <gsaico@icloud.com>
2016-09-23 11:07:28 -0500

    OK funciona con excel  8.0

---
+++ PruebaUnitariaExcelOK/PersistenciaDatos/Usuario.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PersistenciaDatos
+{
+    public class Usuario
+    {
+        [Required]
+        [RegularExpression(@"^.{8,}$", ErrorMessage = "Minimo 8 caracteres")]
+        [StringLength(8, ErrorMessage = "Maximo {8} caracteres")]
+        public string dni { get; set; }
+
+        //En este segundo ejemplo, la longitud de la contraseña no importa, 
+        //pero la contraseña debe contener al menos 1 número, al menos 1 minúscula, y al menos 1 mayúscula.
+        //  ^\w*(?=\w*\d)(?=\w*[a-z])(?=\w*[A-Z])\w*$
+
+        //En este  tercer ejemplo:
+        //Debe tener al menos 10 caracteres
+        //Debe contener al menos uno una letra minúscula, una letra mayúscula, un dígito y un carácter especial
+        //caracteres especiales válidos son - @ # $% ^ & + =
+        //      ^.*(?=.{10,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=]).*$
+
+        //Valida una contraseña segura.Debe tener entre 8 y 10 caracteres, 
+        //contener al menos un dígito y un carácter alfabético, y no debe contener caracteres especiales.
+        //   [RegularExpression(@"(?!^[0-9]*$)(?!^[a-zA-Z]*$)^([a-zA-Z0-9]{8,10})$", ErrorMessage = "La contraseña debe al menos un dígito y un carácter alfabético, y no debe contener caracteres especiales.")]
+        [Required]
+        [RegularExpression(@"^.*(?=.{10,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=]).*$", ErrorMessage = "Debe contener al menos uno una letra minúscula, una letra mayúscula, un dígito y un carácter especial")]
+        public string password { get; set; }
+    }
+}
---
+++ PruebaUnitariaExcelOK/PruebaUnitariaExcelOK/UnitTest1.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PersistenciaDatos;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+namespace PruebaUnitariaExcelOK
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        public TestContext TestContext { get; set; }
+        [TestMethod]
+       // [DeploymentItem("d:\\scratch\\Data.xls")]
+        [DataSource("System.Data.OleDb",
+            "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=d:\\scratch\\Data.xls;Persist Security Info=False;Extended Properties='Excel 8.0'",
+            "Hoja2$", DataAccessMethod.Sequential)]
+        public void TestMethod1()
+        {
+            Usuario add = new Usuario
+            {
+                dni = TestContext.DataRow["DNI"].ToString(),
+                password = TestContext.DataRow["PASSWORD"].ToString()
+            };
+            var lstErrors = ValidateModel(add);
+            Assert.IsTrue(lstErrors.Count == 0);
+        }
+        private IList<ValidationResult> ValidateModel(object model)
+        {
+            var validationResults = new List<ValidationResult>();
+            var ctx = new ValidationContext(model, null, null);
+            Validator.TryValidateObject(model, ctx, validationResults, true);
+            return validationResults;
+        }
+    
+    }
+}
